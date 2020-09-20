037ab298ef340bdbcbf9be8e1df9f5db87f4f980
ymym <>
2020-09-20 21:49:02 +0900

    dark theme

--- js/script.js
+++ js/script.js
@@ -54,3 +54,26 @@ $(function () {
 		$('.circle').toggleClass('circle_full');
 	}, 8000);
 });
+
+$('#toggle').click(function () {
+	// console.log($(this).prop('checked'));
+	if ($(this).prop('checked')) {
+		$('label').css('background-color', '#66cdaa');
+		$('label').removeClass('toggle_button');
+		$('label').addClass('toggle_button_on');
+		$('.wrapper').addClass('wrapper_dark');
+		$('.circle_inline').addClass('circle_inline_dark');
+		$('.circle_outline').addClass('circle_outline_dark');
+		$('.contents').addClass('contents_dark');
+		$('.toggle_text').addClass('toggle_text_dark');
+	} else {
+		$('label').css('background-color', '#ccc');
+		$('label').addClass('toggle_button');
+		$('label').removeClass('toggle_button_on');
+		$('.wrapper').removeClass('wrapper_dark');
+		$('.circle_inline').removeClass('circle_inline_dark');
+		$('.circle_outline').removeClass('circle_outline_dark');
+		$('.contents').removeClass('contents_dark');
+		$('.toggle_text').removeClass('toggle_text_dark');
+	}
+});
